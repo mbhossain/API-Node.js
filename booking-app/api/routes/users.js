a544572eb81942e6f459cee21e4c4adc76c25425
mbhossain <king.cseiu@gmail.com>
2022-06-26 03:33:20 +0600

    check authentication by verify token

--- booking-app/api/routes/users.js
+++ booking-app/api/routes/users.js
@@ -1,7 +1,28 @@
 import express from "express";
+import {
+    updateUser
+    , deleteUser
+    , getUser
+    , getUsers
+} from '../controllers/user.js'
+import { verifyToken } from "../utils/verifyToken.js"
 
 const router = express.Router();
 
+router.get('/checkauthencation', verifyToken, (req, res, next) => {
+    res.send('You are logged in.')
+})
 
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/", getUsers);
 
 export default router
