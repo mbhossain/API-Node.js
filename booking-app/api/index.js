97e045a2a8b10137ebe08a6b501c8eea7faa0003
mbhossain <king.cseiu@gmail.com>
2022-06-25 18:28:56 +0600

    crud Hotel

--- booking-app/api/index.js
+++ booking-app/api/index.js
@@ -2,6 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+
 const app = express()
 
 dotenv.config()
@@ -18,13 +23,20 @@ const connect = async () => {
 mongoose.connection.on('disconnected', () => {
     console.log('mongoDB disconnected!')
 })
-mongoose.connection.on('connected', () => {
-    console.log('mongoDB connected!')
-})
+// mongoose.connection.on('connected', () => {
+//     console.log('mongoDB connected!')
+// })
 
-app.get('/users', (req, res) => {
-    res.send('Hello first request!')
-})
+// app.get('/users', (req, res) => {
+//     res.send('Hello first request!')
+// })
+
+//middlewares
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
 
 app.listen(8800, () => {
     connect()
