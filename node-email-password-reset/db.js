8d73869864e2ecefc8c0f3f23df51b1630152207
mbhossain <king.cseiu@gmail.com>
2023-01-18 18:14:55 +0600

    forgot password project

---
+++ node-email-password-reset/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+module.exports = connection = async () => {
+    try {
+        const connectionParams = {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        };
+        await mongoose.connect(process.env.MONGO, connectionParams);
+        console.log("connected to database.");
+    } catch (error) {
+        
+        console.log(error, "could not connect database.");
+    }
+};
